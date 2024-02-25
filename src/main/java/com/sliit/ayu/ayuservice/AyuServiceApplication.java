372cafb1246baed39ac49a3540ed83b2ee0278e9
dsulochana <damith.sulochana@oloidtechnologies.com>
2024-02-25 09:30:11 +0530

    prolect inititation

---
+++ src/main/java/com/sliit/ayu/ayuservice/AyuServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.ayu.ayuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AyuServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AyuServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sliit/ayu/ayuservice/AyuServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sliit.ayu.ayuservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AyuServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
