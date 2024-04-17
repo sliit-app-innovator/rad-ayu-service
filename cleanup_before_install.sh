e10f7cbe9da22c2368b13d0324f725cef0614a58
Ravindra Warnasooriya <warnasooriya.ravindra@gmail.com>
2024-04-17 14:12:37 +0530

    updated with clean old files

--- cleanup_before_install.sh
+++ cleanup_before_install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-# Clean up the /var/www/html directory
+
+# Find the process ID using the port number 8080
+PID=$(lsof -ti:8080)
+
+# Check if any PID was found
+if [[ -z "$PID" ]]; then
+  echo "No process is running on port 8080."
+else
+  # Kill the process
+  kill -9 $PID
+  if [[ $? -eq 0 ]]; then
+    echo "Process running on port 8080 has been killed."
+  else
+    echo "Failed to kill the process running on port 8080."
+  fi
+fi
+
 rm -rf /home/ubuntu/ayu-app/*
