039316df03ee02febf09d90f60e6e4974f0f35d9
Ravindra Warnasooriya <warnasooriya.ravindra@gmail.com>
2024-04-17 14:58:49 +0530

    updated for cicd

--- start_application.sh
+++ start_application.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/ubuntu/ayu-app/
-nohup java -jar ayu-service-0.0.1.jar >> /home/ayu-app-log/log.txt 2>&1 &
+nohup java -jar ayu-service-0.0.1.jar >> /home/ubuntu/ayu-app-log/log.txt 2>&1 &
 
